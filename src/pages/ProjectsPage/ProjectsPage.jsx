450d98f35840047ca47aeb57def032bc136de4f2
AliceTYu <turaeva.alice@gmail.com>
2024-10-08 13:11:46 +0300

    fix

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -3,10 +3,11 @@ import CardProject from './../../components/CardProject/CardProject';
 
 const ARRAY_PROJECT = [
 	{id: 1, title: 'Портфолио', skills: 'React(Vite), JavaScript', images: './react-profile.png', demoLink: 'https://alicetyu.github.io/react-profile/', gitLink: 'https://github.com/AliceTYu/react-profile'},
-	{id: 2, title: 'Крестики нолики', skills: 'React(Vite), JavaScript', images: './crossAndToe.png', demoLink: 'https://alicetyu.github.io/crossAndToe/', gitLink: 'https://github.com/AliceTYu/crossAndToe'},
-	{id: 3, title: 'Журнал', skills: 'React(Vite), JavaScript', images: './react-journal-pay.png', demoLink: 'https://alicetyu.github.io/journal-for-me/', gitLink: 'https://github.com/AliceTYu/journal-for-me'},
-	{id: 4, title: 'Магазин', skills: 'React(Vite), TypeScript', images: './react-vite-ts-shop.png', demoLink: 'https://alicetyu.github.io/react-vite-ts-shop/', gitLink: 'https://github.com/AliceTYu/react-vite-ts-shop'}
-	//{id: 5, title: 'Каталог фильмов', skills: 'React(Vite), TypeScript', images: './', demoLink: '', gitLink: ''}
+	{id: 2, title: 'Трекер привычек', skills: 'JavaScript', images: './habit_tracker.png', demoLink: 'https://alicetyu.github.io/habit_tracker/', gitLink: 'https://github.com/AliceTYu/habit_tracker'},
+	{id: 3, title: 'Крестики нолики', skills: 'React(Vite), JavaScript', images: './crossAndToe.png', demoLink: 'https://alicetyu.github.io/crossAndToe/', gitLink: 'https://github.com/AliceTYu/crossAndToe'},
+	{id: 4, title: 'Журнал', skills: 'React(Vite), JavaScript', images: './react-journal-pay.png', demoLink: 'https://alicetyu.github.io/journal-for-me/', gitLink: 'https://github.com/AliceTYu/journal-for-me'},
+	{id: 5, title: 'Магазин (в процессе)', skills: 'React(Vite), TypeScript', images: './react-vite-ts-shop.png', demoLink: 'https://alicetyu.github.io/react-vite-ts-shop/', gitLink: 'https://github.com/AliceTYu/react-vite-ts-shop'}
+	//{id: 6, title: 'Каталог фильмов', skills: 'React(Vite), TypeScript', images: './', demoLink: '', gitLink: ''}
 ];
 
 function ProjectsPage() {
